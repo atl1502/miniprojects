a0e43e7300708f1aa162bad24213d7f36571463d
Andrew Li <atl15c02@gmail.com>
2020-09-07 19:00:35 -0500

    frequnecy cracking done

--- CodeCracker/CodeCracker.py
+++ CodeCracker/CodeCracker.py
@@ -1,22 +1,47 @@
 import json
-# test dictonaries
-dict = {
-    "first": 1,
-    "second": 2,
-    "fourth": 4,
-    "third":  3,
-    "seventh":  7,
-    "fifth":  5,
-    "eigth":  8,
-    "sixth":  6
-}
-dict2 = {
-    "first": 3,
-    "secound": 2,
-    "third": 1
-}
 # actual variables
-msg = "ruaghjdfjbhadfxjgraoifdj fdjkng sjd"
+
+msg = """Bzdeai k nem sqob ydeod tqqbzi iqya qhzs pdz yeiz, lpskmmteam sqki pdkp ska cxyksi pdsqcmd pdz wqcapkeal osqcodzi k tqam, tzka jemcsz. Sacmmtzi kmkealp del semdp ldqctizs ykl k sejtz, kai pdz nzksizi jkoz nzazkpd pdz nsqki-nsewwzi xkakwk ykl pcsazi pqyksi pdz sqkiyku nztqy. Tdz dqp lca nzkp iqya szwqslztzlltu, kai epl nteaieam skul yzsz szjtzopzi jsqw pdz sqobl. Pzslxeskpeqa xqcszi iqya pdz wka’l jkoz, kai aqy kai pdza dz wqhzi ewxkpezaptu pq nscld kyku lqwz ncffeam ealzop. Hel dzki ykl skelzi ltemdptu knqhz pdz tzhzt qj pdz sqob, kai jsqw del xqeap qj hkapkmz k lxtzaiei xkaqskwk lxszki qcp nzazkpd dew.
+
+Tq del tzjp tku pdz wqcapkeal, ntcz, szwqpz, kai jctt qj scmmzi iemaepu ktt pdzes qya. Tq del semdp, k jzspetz Sqcpd Awzseoka hkttzu szhzktzi eplztj ea pdz ldewwzseam ielpkaoz. Oookleqakttu, kl k temdp xcjj qj yeai okwz cx jsqw pdz tqytkail, ep nsqcmdp yepd ep pdz ictt, dzkhu aqelz qj ka zameaz kp yqsb.
+
+Hktj ka dqcs xkllzi, kai pdza pdz jeslp lema qj tejz ykl szhzktzi ea pdz sqkiyku nztqy. Tdzsz kxxzkszi sqcai k nzai k tqam teaz qj wctzl, zkod qj pdzw ncsizazi yepd pyq nem xkobl. Ia jsqap qj pdz[6] pskea qj wctzl yktbzi k ydepz wka otki ea ieamu qhzskttl.
+
+Tdz jemcsz osqcodeam nzdeai pdz sqob wqhzi ltemdptu kai lzzwzi pq msqy pzalz kai zrxzopkap, ydetz pdz zuzl ea pdz nzksizi jkoz mteapzi kl pdzu xzzszi iqya kp pdz sqki.
+
+Nqpdeam dkxxzazi, dqyzhzs. Tdz wctzl xtqiizi qa, yepd pdzes tzkizs lpseieam kyku kdzki qj pdzw, kai pdz tqaztu lzapeazt ykpodzi pdzw capet pdzu dki xkllzi iqya pdz sqki kai dki hkaeldzi nztqy pdz tzhzt qj pdz selz ydeod tzi pdzw qa pq pdz xtkeal.
+
+“Hz qcmdp pq nz oqweam lqqa aqy.”
+
+Tdz wka lxqbz ktqci, kai pdzsz ykl k ocseqcl, wzpktteo lqcai ea del sklxeam hqeoz.
+
+Tza weacpzl xkllzi, kai pdza pdz otzks, iscwweam lqcai qj k dqslz’l dqqjl okwz pq dew, kai xszlzaptu ksqcai pdz lkwz gkmmzi lxcs pdzsz kxxzkszi pdz jemcsz qj k wka qa dqslznkob. Hz ykl seieam ktqam kp k mqqi xkoz, ncp pdz szeal yzsz tueam tqqlztu kosqll pdz dqslz’l azob, kai pdz kaewkt ykl xeobeam epl yku camceizi iqya pdz sqcmd lcsjkoz qj pdz sqki. Eheizaptu ep ykl qa k jkweteks psket.
+
+Ap pdz lemdp, pdz tcsbeam jemcsz mszy pzalzs lpett, kai pdz lqcai qj k tqy msqyt, ktwqlp kaewkt-tebz ea odkskopzs, wemdp dkhz nzza dzksi. Stqytu pdz sejtz ykl azlptzi otqlzs pq pdz ldqctizs. Tdz xkakwk dkp, nzeam pqq oqalxeocqcl, ykl xcttzi qjj kai isqxxzi nzdeai dew, kjpzs ydeod pdz nksz, skpdzs nkti dzki ykl tqyzszi capet pdz semdp odzzb pqcodzi pdz lpqob qj pdz mca. Tdz tzjp zuz ykl otqlzi, kai pdz semdp[7] lemdpzi ktqam pdz nksszt, ydeod kp pdz lkwz pewz ykl ldejpzi, jqttqyeam pdz wka qa dqslznkob.
+
+A jzy wqwzapl xkllzi, icseam ydeod pdz iqya-xqeapzi wcfftz ldejpzi tebz k lxu-mtkll, jqttqyeam pdz wqheam qngzop. Tdza——
+
+Cskob! Iapq pdz lpett kes k ntcz xcjj qj lwqbz sqlz kai dcam jqs k wqwzap knqhz pdz sqob. Tdz isqaz qj pdz ncttzp lqcaizi otzkstu iqya pdz zimz qj pdz ltqxz kl pdz izkitu welletz dcstzi eplztj pqyksi epl wksb. A vceob osu okwz cx jsqw pdz sqkiyku, kai pdz yzkxqa ykl lpzktpdetu yepdiskya.
+
+Qceobtu, dqyzhzs, pdz wka nzdeai pdz sqob xzzszi iqya, ncp ydza dz iei lq dz lky pdkp nteai odkaoz dki lpzxxzi ea kai pdykspzi dew. Tdz dqslz dki kxxkszaptu lpcwntzi qa k tqqlz sqob gclp kl pdz ldqp ykl jeszi, kai dki szkszi nkob ltemdptu pq szoqhzs epl jqqpeam; pdzszjqsz, ep ykl eapq pdz kaewkt’l lqjp, sqcaizi azob pdkp pdz dzkhu ncttzp dki nqszi epl yku, kai aqp eapq pdz wqsz xszoeqcl pksmzp kp ydeod ep dki nzza kewzi.
+
+Tdz oszkpcsz ykl aqy tueam ea pdz sqkiyku, kai pdz oqahctlehz wqhzwzapl qj epl tewnl oqcti nz lzza iewtu pdsqcmd pdz tepptz otqci qj iclp ydeod dki nzza skelzi nu epl jktt.
+
+Tdz wka qa pdz dqslz’l nkob dki nzza dcstzi ea k dzkx nu pdz leiz qj pdz sqki, ncp kl del yqcti-nz wcsizszs ykpodzi, dz lky dew selz pq del jzzp kai lpksz cx ea pdz mzazskt ieszopeqa qj pdz sqob jsqw ydeod pdz ldqp dki nzza jeszi. Wksazi nu pdkp wqhzwzap, pdz lbctbzs lyejptu gzsbzi del dzki nkob kai osqcodzi lpett tqyzs ea del xtkoz.
+
+[8]
+
+“Ccslz dew!” pdz dksi hqeoz mskpzi. “Hz ktykul dkl pdz jezai’l qya tcob!”
+
+Gsklxeam del sejtz kai dkp, ncp lpett bzzxeam qa dkail kai bazzl, dz nzmka ealpeaopehztu pq oskyt kyku caizs oqhzs qj pdz sqob. Hz dki mqaz aq wqsz pdka k uksi, pdqcmd, nzjqsz dz xkclzi esszlqtcpztu kai del jeamzsl lqcmdp del nztp.
+
+Tdzsz yzsz qpdzs ncttzpl ea pdkp nztp, ncp pdz wka’l jketcsz dki caazshzi dew, kai k ozspkea jkpktelpeo ealpeaop pqti dew pdkp dz ykl aqp tebztu pq lcoozzi ea k lzoqai kppzwxp, aqy pdkp pdz jeslp dki oqwz pq akcmdp. Tdz jemcsz ea pdz sqki yqcti nz qa epl mcksi aqy, kai ej kaqpdzs ldqp wellzi epl wksb, pdz xqeap jsqw ydeod ep dki nzza jeszi yqcti ktwqlp ozspkeatu nz tqokpzi. Fsqw pdkp yqcti qatu nz k lpzx pq pdz ieloqhzsu qj pdz ldqqpzs’l eizapepu, kai pdz tkppzs iei aqp oksz pq oqapzwxtkpz lcod k xqllenetepu. Cqalzvczaptu, yepd k lakbztebz wqhzwzap, pdz tzka jemcsz szlcwzi epl xsqmszll kyku jsqw pdz sqobl, kai xszlzaptu, dkheam szkodzi pdz xsqpzopeqa qj tksmz nqytizsl, lpskemdpzazi cx k tepptz wqsz kai eaoszklzi epl xkoz.
+
+Tdz jcmepehz bazy pdkp pdz wka dz dki psezi pq bett ykl wqsz pdka clckttu jqai qj pdz iueam dqslz, kai yqcti xsqnkntu iztku kp epl leiz jqs k xszoeqcl weacpz qs pyq nzjqsz kppzwxpeam pq lqthz pdz wulpzsu qj pdz ldqp. Tdkp iztku xsqwelzi pq zakntz dew pq wkbz mqqi del zlokxz, kai dz ykl szlqthzi pq pkbz zhzsu xqllentz kihkapkmz qj ep. Fqs xzsdkxl jejpzza weacpzl dz iqcntzi kai pyelpzi, aqy klozaieam kai aqy izlozaieam pdz jqqpdettl. Ap pdz zai qj pdkp pewz dz dki szkodzi pdz sqki kmkea, kai, ykpodeam[9] del odkaoz, iqimzi kosqll ep. Tdel tkpzlp wqhz nsqcmdp dew eapq pdeob yqqil, pdsqcmd ydeod dz dcssezitu pdszkizi del yku ea pdz ieszopeqa qj pdz hkttzu.
+
+Hz dei del sejtz ea k dqttqy pszz, kai ydza dz szkodzi pdz tepptz weaeam okwx dz dki ocaaeamtu oqaozktzi ktt zheizaoz qj kmepkpeqa qs mcetp.
+"""
+
 msg_frequency = {
     "a": 0,
     "b": 0,
@@ -77,22 +102,46 @@ def sort(array):
 
 
 def get_frequency(message):
+    number_of_letters = 0
     message = message.lower()
     for key in msg_frequency:
         for letter in message:
             if key == letter:
                 msg_frequency[key] += 1
+                number_of_letters += 1
+    for key in msg_frequency:
+        msg_frequency[key] = (msg_frequency[key] / number_of_letters) * 100
 
-# compares one array frequency with the other and stores frequencies in order.
-
-
-def frequency_matcher(real_values, coded_values):
-    array_keys = []
-    for i in real_values:
-        array_keys.append((real_values[i][1], coded_values[i][1]))
-    return array_keys
+# compares array frequency with the real frequencies and stores frequencies in order.
 
 
-get_frequency(msg)
-print(msg_frequency)
-print(frequency_sorter(dict))
+def frequency_matcher(coded_values):
+    key_dict = {}
+    with open('/Users/andrewli/Documents/Code Stuff/Mini_Projects/CodeCracker/Data.json', 'r') as f:
+        real_values = frequency_sorter(json.loads(f.read()))
+        for i in range(len(real_values)):
+            key_dict[real_values[i][0]] = coded_values[i][0]
+    return key_dict
+# input dict in form of "actual value = code value" replaces the letter with new value
+
+
+def frequency_decode(dict, msg):
+    decoded_msg = ""
+    lower_msg = msg.lower()
+    for character in lower_msg:
+        for key in dict:
+            if character == dict[key]:
+                decoded_msg += key
+                break
+        else:
+            decoded_msg += character
+    return decoded_msg
+
+
+def decode(msg):
+    get_frequency(msg)
+    print(frequency_decode(frequency_matcher(
+        frequency_sorter(msg_frequency)), msg))
+
+
+decode(msg)
--- CodeCracker/Coder.py
+++ CodeCracker/Coder.py
@@ -1,5 +1,63 @@
 import random
-msg = """hello this is a test of the message scrambling"""
+msg = """Behind a big rock which looked down over the wide, straggling road that ran upward through the mountains crouched a long, lean figure. Snuggled against his right shoulder was a rifle, and the bearded face beneath the broad-brimmed panama was turned toward the roadway below. The hot sun beat down remorselessly, and its blinding rays were reflected from the rocks. Perspiration poured down the man’s face, and now and then he moved impatiently to brush away some buzzing insect. His head was raised slightly above the level of the rock, and from his point of vantage a splendid panorama spread out beneath him.
+
+To his left lay the mountains, blue, remote, and full of rugged dignity all their own. To his right, a fertile South American valley revealed itself in the shimmering distance. Occasionally, as a light puff of wind came up from the lowlands, it brought with it the dull, heavy noise of an engine at work.
+
+Half an hour passed, and then the first sign of life was revealed in the roadway below. There appeared round a bend a long line of mules, each of them burdened with two big packs. In front of the[6] train of mules walked a white man clad in dingy overalls.
+
+The figure crouching behind the rock moved slightly and seemed to grow tense and expectant, while the eyes in the bearded face glinted as they peered down at the road.
+
+Nothing happened, however. The mules plodded on, with their leader striding away ahead of them, and the lonely sentinel watched them until they had passed down the road and had vanished below the level of the rise which led them on to the plains.
+
+“He ought to be coming soon now.”
+
+The man spoke aloud, and there was a curious, metallic sound in his rasping voice.
+
+Ten minutes passed, and then the clear, drumming sound of a horse’s hoofs came to him, and presently around the same jagged spur there appeared the figure of a man on horseback. He was riding along at a good pace, but the reins were lying loosely across the horse’s neck, and the animal was picking its way unguided down the rough surface of the road. Evidently it was on a familiar trail.
+
+At the sight, the lurking figure grew tenser still, and the sound of a low growl, almost animal-like in character, might have been heard. Slowly the rifle was nestled closer to the shoulder. The panama hat, being too conspicuous, was pulled off and dropped behind him, after which the bare, rather bald head was lowered until the right cheek touched the stock of the gun. The left eye was closed, and the right[7] sighted along the barrel, which at the same time was shifted, following the man on horseback.
+
+A few moments passed, during which the down-pointed muzzle shifted like a spy-glass, following the moving object. Then——
+
+Crack! Into the still air a blue puff of smoke rose and hung for a moment above the rock. The drone of the bullet sounded clearly down the edge of the slope as the deadly missile hurled itself toward its mark. A quick cry came up from the roadway, and the weapon was stealthily withdrawn.
+
+Quickly, however, the man behind the rock peered down, but when he did so he saw that blind chance had stepped in and thwarted him. The horse had apparently stumbled on a loose rock just as the shot was fired, and had reared back slightly to recover its footing; therefore, it was into the animal’s soft, rounded neck that the heavy bullet had bored its way, and not into the more precious target at which it had been aimed.
+
+The creature was now lying in the roadway, and the convulsive movements of its limbs could be seen dimly through the little cloud of dust which had been raised by its fall.
+
+The man on the horse’s back had been hurled in a heap by the side of the road, but as his would-be murderer watched, he saw him rise to his feet and stare up in the general direction of the rock from which the shot had been fired. Warned by that movement, the skulker swiftly jerked his head back and crouched still lower in his place.
+
+[8]
+
+“Curse him!” the hard voice grated. “He always has the fiend’s own luck!”
+
+Grasping his rifle and hat, but still keeping on hands and knees, he began instinctively to crawl away under cover of the rock. He had gone no more than a yard, though, before he paused irresolutely and his fingers sought his belt.
+
+There were other bullets in that belt, but the man’s failure had unnerved him, and a certain fatalistic instinct told him that he was not likely to succeed in a second attempt, now that the first had come to naught. The figure in the road would be on its guard now, and if another shot missed its mark, the point from which it had been fired would almost certainly be located. From that would only be a step to the discovery of the shooter’s identity, and the latter did not care to contemplate such a possibility. Consequently, with a snakelike movement, the lean figure resumed its progress away from the rocks, and presently, having reached the protection of large bowlders, straightened up a little more and increased its pace.
+
+The fugitive knew that the man he had tried to kill was more than usually fond of the dying horse, and would probably delay at its side for a precious minute or two before attempting to solve the mystery of the shot. That delay promised to enable him to make good his escape, and he was resolved to take every possible advantage of it. For perhaps fifteen minutes he doubled and twisted, now ascending and now descending the foothills. At the end of that time he had reached the road again, and, watching[9] his chance, dodged across it. This latest move brought him into thick woods, through which he hurriedly threaded his way in the direction of the valley.
+
+He hid his rifle in a hollow tree, and when he reached the little mining camp he had cunningly concealed all evidence of agitation or guilt.
+
+The knowledge of the act was not destined to remain locked in his own breast, however, as he was soon to learn. At his destination, the Condor Mine, he found Charlie Floyd, the mine’s physician, waiting for him, and wearing a very stern expression.
+
+“I have something important to say to you, Mr. Stone,” the young doctor said grimly, and led the way to a spot where they were out of earshot.
+
+“What’s up?” demanded Stone, who was one of the two original owners of the mine. He and his partner, Winthrop Crawford, had only recently sold out for a cool million.
+
+“Much,” was the grave answer. “I happened to be roaming about in the foothills back there a little while ago, and I saw you take that pot shot at Mr. Crawford.”
+
+“What are you raving about?” growled Stone, with the greatest apparent surprise.
+
+“I’m not raving at all, Mr. Stone. I always carry field glasses on my walks, as you know, and, being startled by the shot, I looked in that direction, saw the puff of smoke from behind the rock, and leveled my glasses on the spot. I saw you when you looked down to see if the bullet had done its work; saw you as plainly as if you had been not more than ten feet[10] away. There’s no possibility of a mistake. I was in a position to watch your movements afterward, and saw you sneaking away. I recognized your hat, too.”
+
+Stone had wilted at first when the field glasses were mentioned, but now he seemed to have plucked up fresh courage, and even assumed a defiant attitude.
+
+“Well, what are you going to do about it?” he demanded. “One or the other of us will have to kick the bucket sooner or later. Crawford has it in for me, and I only acted in self-defense. If I don’t get him first, he’ll get me as sure as fate.”
+
+The young physician looked at him searchingly, but there was much more of pity than condemnation in his glance.
+
+“You needn’t be afraid that I’m going to give you up to justice, Mr. Stone,” he said, after a pause. “You’ll resent it, of course, but I’m pretty sure that you’re not responsible for your actions. I hold your liberty, if not your life, in my hands, though, and I’m going to name a condition in return for my silence.”"""
 
 # returns scrambled alphabet and I was too lazy to convert into array
 
